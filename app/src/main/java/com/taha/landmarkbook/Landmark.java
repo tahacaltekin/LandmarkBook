9a138bc4ecdc5c524ff386731a4c2f57dc2290a7
tahacaltekin <thcaltekin@gmail.com>
2021-10-08 22:55:52 +0300

    LandmarkBook

---
+++ app/src/main/java/com/taha/landmarkbook/Landmark.java
@@ -0,0 +1,18 @@
+package com.taha.landmarkbook;
+
+import java.io.Serializable;
+
+public class Landmark implements Serializable {
+
+    String name;
+    String country;
+    int image;
+
+    //Constructor
+
+    public Landmark(String name, String country, int image) {
+        this.name = name;
+        this.country = country;
+        this.image = image;
+    }
+}
